9b117688ca0c9046d7263884e4d5c120769e4fee
Jonas Trevisan <jonhkr@gmail.com>
2025-12-02 16:39:43 -0300

    use regex to make day 2 faster

--- 2/code.py
+++ 2/code.py
@@ -1,44 +1,25 @@
+import re
+
+PART_1_PATTERN = re.compile(r"^(.+)\1$")
+PART_2_PATTERN = re.compile(r"^(.+)\1+$")
 
 def valid_p1(_id):
-	if len(_id) == 1:
-		return True
-
-	if len(_id) == 2:
-		return _id[0] != _id[1]
-
-	if len(_id) % 2 == 1:
-		return True
-
-	mid = len(_id) // 2
-	a = _id[:mid]
-	b = _id[mid:]
-	return a != b
+	return PART_1_PATTERN.match(_id) is None
 
 def valid_p2(_id):
-	if len(_id) == 1:
-		return True
-
-	if len(_id) == 2:
-		return _id[0] != _id[1]
-
-	mid = len(_id) // 2
-	for i in range(1, mid + 1):
-		w = _id[0:i]
-		if _id.count(w) == len(_id) / len(w):
-			return False
-	return True
+	return PART_2_PATTERN.match(_id) is None
 
 with open('input.txt', 'r') as i:
 	ranges = i.read().split(',')
-	invalids_p1 = []
-	invalids_p2 = []
+	invalids_p1 = 0
+	invalids_p2 = 0
 	for r in ranges:
 		[a, b] = r.split('-')
 		for i in range(int(a), int(b) + 1):
 			if not valid_p1(str(i)):
-				invalids_p1.append(i)
+				invalids_p1 += i
 			if not valid_p2(str(i)):
-				invalids_p2.append(i)
+				invalids_p2 += i
 	
-	print("Part 1:", sum(invalids_p1))
-	print("Part 2:", sum(invalids_p2))
+	print("Part 1:", invalids_p1)
+	print("Part 2:", invalids_p2)
