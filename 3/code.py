d272fbc27ed0300f122118e15870dc02dec3ef4c
Jonas Trevisan <jonhkr@gmail.com>
2025-12-03 10:32:39 -0300

    add day 3 part 2

--- 3/code.py
+++ 3/code.py
@@ -1,24 +1,36 @@
 import re
 
+def joltage(bats, n):
+	bat_d = '0'
+	bat_v = 0
+	bat_i = 0
+	for i, d in enumerate(bats):
+		v = int(d)
+		
+		if v > bat_v:
+			bat_d = d
+			bat_v = v
+			bat_i = i
+
+		if i + n == len(bats):
+			if n == 1:
+				return bat_d
+			else:
+				return bat_d + joltage(bats[bat_i+1:], n-1)
+
+
 with open('input.txt', 'r') as file:
-	total_joltage = 0
+	part_1 = 0
+	part_2 = 0
 	for line in file:
 		line = line.strip()
-		bat_1 = 0
-		bat_2 = 0
-		l = len(line)
 		
-		for i in range(0, l): 
-			d = int(line[i])
-			if d > bat_1 and i+1 != l:
-				bat_1 = d
-				bat_2 = int(line[i+1])
-			else:
-				if d > bat_2:
-					bat_2 = d
+		jolt_2 = joltage(line, 2)
+		jolt_12 = joltage(line, 12)
 		
-		joltage = int(str(bat_1) + str(bat_2))
-		total_joltage += joltage
-		print(line, bat_1, bat_2, joltage, total_joltage)
-		
+		part_1 += int(jolt_2)
+		part_2 += int(jolt_12)
+	
+	print("Part 1:", part_1)
+	print("Part 2:", part_2)		
 		
