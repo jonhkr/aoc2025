cd543f26ef460928ba63d866565597ed91e04463
Jonas Trevisan <jonhkr@gmail.com>
2025-12-12 23:19:02 -0300

    add solution to day 10 part 2

--- 10/code.java
+++ 10/code.java
@@ -1,29 +1,9 @@
+import com.microsoft.z3.*;
 
-record Button(Set<Integer> lights) {}
-record Machine(Set<Integer> lights, Set<Button> buttons, List<Integer> joltage) {}
+import java.util.regex.Pattern;
 
-Set<Integer> setXor(Set<Integer> a, Set<Integer> b) {
-    var out = new HashSet<Integer>();
-    for (Integer i : a) {
-        if (!b.contains(i)) {
-            out.add(i);
-        }
-    }
-
-    for (Integer i : b) {
-        if (!a.contains(i)) {
-            out.add(i);
-        }
-    }
-    return out;
-}
-
-void testSetXor() {
-    var r = setXor(Set.of(1, 2, 3), Set.of(0,3,4));
-    if (!Set.of(0,1,2,4).equals(r)) {
-        throw new AssertionError();
-    }
-}
+record Button(Set<Integer> lights, int bits) {}
+record Machine(int lightBits, Set<Button> buttons, List<Integer> joltage) {}
 
 Set<Set<Button>> combinations(Set<Button> buttons, int count) {
     var list = new ArrayList<>(buttons);
@@ -51,41 +31,81 @@ private void backtrack(List<Button> buttons, int count,
 }
 
 void testCombinations() {
-    var r = Set.of(new Button(Set.of(1, 2)), new Button(Set.of(0,3)), new Button(Set.of(3)));
+    var r = Set.of(new Button(Set.of(1, 2),0), new Button(Set.of(0,3),0), new Button(Set.of(3),0));
     var c = combinations(r, 2);
     if (!Set.of(
-            Set.of(new Button(Set.of(1, 2)), new Button(Set.of(0, 3))),
-            Set.of(new Button(Set.of(1, 2)), new Button(Set.of(3))),
-            Set.of(new Button(Set.of(0, 3)), new Button(Set.of(3)))
+            Set.of(new Button(Set.of(1, 2),0), new Button(Set.of(0, 3),0)),
+            Set.of(new Button(Set.of(1, 2),0), new Button(Set.of(3),0)),
+            Set.of(new Button(Set.of(0, 3),0), new Button(Set.of(3),0))
     ).equals(c)) {
         throw new AssertionError();
     }
 }
 
 void main() {
-    testSetXor();
     testCombinations();
 
     var in = new BufferedReader(new InputStreamReader(System.in));
     var machines = parseInput(in);
     var total = 0L;
+    var part2 = 0L;
     for (var m : machines) {
         done:
         for (var c : IntStream.range(1, m.buttons().size() + 1).toArray()) {
             for (var attempt : combinations(m.buttons(), c)) {
-                var lights = Set.<Integer>of();
+                var lightBits = 0L;
                 for(var button : attempt) {
-                    lights = setXor(lights, button.lights);
+                    lightBits ^= button.bits;
                 }
-
-                if (lights.equals(m.lights)) {
+                if (m.lightBits == lightBits) {
                     total += c;
                     break done;
                 }
             }
         }
+
+        HashMap<String, String> cfg = new HashMap<>();
+        cfg.put("model", "true");
+
+        try (var ctx = new Context(cfg)) {
+            var o = ctx.mkOptimize();
+            var vars = new ArrayList<IntExpr>();
+            var buttons = new ArrayList<>(m.buttons());
+            for (int i = 0; i < buttons.size(); i++) {
+                var v = ctx.mkIntConst("B" + i);
+                o.Add(ctx.mkGe(v, ctx.mkInt(0)));
+                vars.add(v);
+            }
+            for (int i = 0; i < m.joltage().size(); i++) {
+                var joltage = m.joltage().get(i);
+                var equation = new ArrayList<IntExpr>();
+                for (int j = 0; j < buttons.size(); j++) {
+                    var button = buttons.get(j);
+                    if (button.lights().contains(i)) {
+                        equation.add(vars.get(j));
+                    }
+                }
+                var e = ctx.mkAdd(equation.toArray(IntExpr[]::new));
+                o.Add(ctx.mkEq(ctx.mkAdd(e), ctx.mkInt(joltage)));
+            }
+            var min = o.MkMinimize(ctx.mkAdd(vars.toArray(IntExpr[]::new)));
+            if (Status.UNSATISFIABLE.equals(o.Check())) {
+                throw new AssertionError();
+            }
+
+            part2 += Integer.parseInt(min.toString());
+        }
     }
+
     print("Part 1:", total);
+
+    /*
+     * Requires Z3 lib
+     * Download from https://github.com/Z3Prover/z3/releases
+     * Copy the bin here
+     * run: $ java -cp com.microsoft.z3.jar code.java < input.txt
+     */
+    print("Part 2:", part2);
 }
 
 List<Machine> parseInput(BufferedReader in) {
@@ -98,23 +118,24 @@ List<Machine> parseInput(BufferedReader in) {
                     throw new IllegalArgumentException();
                 }
                 var lights = matcher.group(1);
-                var l = IntStream.range(0, lights.length())
-                        .filter(i -> lights.charAt(i) == '#')
-                        .boxed()
-                        .collect(Collectors.toSet());
+                var lightBits = IntStream.range(0, lights.length())
+                        .map(i -> lights.charAt(i) == '#' ? Math.powExact(2, i) : 0).sum();
 
                 var buttons = matcher.group(2);
                 var b = Arrays.stream(buttons.split(" "))
-                        .map(b1 -> new Button(
-                                Arrays.stream(b1.substring(1, b1.length()-1)
-                                                .split(","))
-                                        .mapToInt(Integer::parseInt)
-                                        .boxed()
-                                        .collect(Collectors.toSet())))
+                        .map(b1 -> {
+                            var ints = Arrays.stream(b1.substring(1, b1.length()-1)
+                                            .split(","))
+                                    .mapToInt(Integer::parseInt)
+                                    .boxed()
+                                    .collect(Collectors.toSet());
+                            var bits = ints.stream().mapToInt(i -> Math.powExact(2, i)).sum();
+                            return new Button(ints, bits);
+                        })
                         .collect(Collectors.toSet());
                 var j = Arrays.stream(matcher.group(3).split(",")).mapToInt(Integer::parseInt)
                         .boxed().toList();
-                return new Machine(l, b, j);
+                return new Machine(lightBits, b, j);
             })
             .toList();
 }
