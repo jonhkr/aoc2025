02318bace1d69a3af1bb9a8e2eaa5112d4050460
Jonas Trevisan <jonhkr@gmail.com>
2025-12-21 19:57:54 -0300

    part 10 more efficient

--- 10/code.java
+++ 10/code.java
@@ -1,5 +1,3 @@
-import com.microsoft.z3.*;
-
 import java.util.regex.Pattern;
 
 record Button(Set<Integer> lights, int bits) {}
@@ -11,10 +9,10 @@ void main() {
     var machines = parseInput(in);
     var part1 = 0L;
     var part2 = 0L;
+    var solver = new Part2Solver();
     for (var m : machines) {
-
         part1 += configureLights(m);
-        part2 += solveWithLA(m);
+        part2 += solver.solve(m);
     }
 
     print("Part 1:", part1);
@@ -44,184 +42,145 @@ private int configureLights(Machine m) {
     throw new AssertionError("Could not solve");
 }
 
-private double[][] buildAugmentedMatrix(Machine m) {
-    var rows = m.joltage.size();
-    var cols = m.buttons.size() + 1;
-    var augmented = new double[rows][cols];
-    for (var col = 0; col < cols -1; col++) {
-        var button = m.buttons.get(col);
-        for (var b : button.lights) {
-            augmented[b][col] = 1.0;
-        }
-    }
-    for (var row = 0; row < rows; row++) {
-        augmented[row][cols - 1] = (double) m.joltage.get(row);
-    }
-    return augmented;
-}
+static class Part2Solver {
+    private static final double EPSILON = 1e-9;
+    private List<Integer> freeVars = null;
+    private List<Integer> dependentVars = null;
+    private Integer cols = null;
 
-static double EPSILON = 1e-9;
-
-private int solveWithLA(Machine m) {
-    var original = buildAugmentedMatrix(m);
-    var rows = original.length;
-    var cols = original[0].length -1;
-
-    var augmented = new double[rows][cols];
-    for (var row = 0; row < rows; row++) {
-        augmented[row] = original[row].clone();
+    private int solve(Machine m) {
+        var A = buildAugmentedMatrix(m);
+        runGaussianElimination(A);
+        return minimize(A, new double[freeVars.size()], 0, max(m.joltage) + 1);
     }
 
-    var freeVars = new ArrayList<Integer>();
+    private void runGaussianElimination(double[][] A) {
+        var rows = A.length;
+        cols = A[0].length - 1;
+        freeVars = new ArrayList<>();
+        dependentVars = new ArrayList<>();
 
-    var row = 0;
-    var col = 0;
-    while (col < cols && row < rows) {
-        var pivot = row;
-        for (var i = row + 1; i < rows; i++) {
-            if (Math.abs(augmented[i][col])
-                    > Math.abs(augmented[pivot][col])) {
-                pivot = i;
+        var row = 0;
+        var col = 0;
+        while (col < cols && row < rows) {
+            var pivot = row;
+            for (var i = row + 1; i < rows; i++) {
+                if (Math.abs(A[i][col]) > Math.abs(A[pivot][col])) {
+                    pivot = i;
+                }
             }
-        }
 
-        if (Math.abs(augmented[pivot][col]) < EPSILON) {
-            freeVars.add(col);
+            if (Math.abs(A[pivot][col]) < EPSILON) {
+                freeVars.add(col);
+                col++;
+                continue;
+            }
+
+            var temp = A[row];
+            A[row] = A[pivot];
+            A[pivot] = temp;
+            dependentVars.add(col);
+
+            var pivotVal = A[row][col];
+            for (var i = col; i <= cols; i++) {
+                A[row][i] /= pivotVal;
+            }
+
+            for (var i = 0; i < rows; i++) {
+                if (i != row) {
+                    var factor = A[i][col];
+                    for (var j = col; j <= cols; j++) {
+                        A[i][j] -= factor * A[row][j];
+                    }
+                }
+            }
+
+            row++;
             col++;
-            continue;
         }
 
-        var temp = augmented[row];
-        augmented[row] = augmented[pivot];
-        augmented[pivot] = temp;
+        for (var i = col; i < cols; i++) {
+            freeVars.add(i);
+        }
+    }
 
-        var pivotVal = augmented[row][col];
-        for (var i = col; i <= cols; i++) {
-            augmented[row][i] /= pivotVal;
+    private double[][] buildAugmentedMatrix(Machine m) {
+        var rows = m.joltage.size();
+        var cols = m.buttons.size() + 1;
+        var A = new double[rows][cols];
+        for (var col = 0; col < cols - 1; col++) {
+            var button = m.buttons.get(col);
+            for (var b : button.lights) {
+                A[b][col] = 1.0;
+            }
         }
 
-        for (var i = 0; i < rows; i++) {
-            if (i != row) {
-                var factor = augmented[i][col];
-                for (var j = col; j <= cols; j++) {
-                    augmented[i][j] -= factor * augmented[row][j];
+        for (var row = 0; row < rows; row++) {
+            A[row][cols - 1] = (double) m.joltage.get(row);
+        }
+
+        return A;
+    }
+
+    int minimize(double[][] A, double[] freeValues, int depth, int max) {
+        if (freeVars.size() == depth) {
+            var total = sum(freeValues);
+
+            for (int row = 0; row < dependentVars.size(); row++) {
+                var c = A[row][cols];
+                for (var j = 0; j < freeVars.size(); j++) {
+                    var col = freeVars.get(j);
+                    c -= A[row][col] * freeValues[j];
                 }
-            }
-        }
 
-        row++;
-        col++;
-    }
-
-    for (var i = col; i < cols; i++) {
-        freeVars.add(i);
-    }
-
-    var max = m.joltage.stream().mapToInt(i -> i).max().orElseThrow() + 1;
-    var varValues = findMissingVariables(augmented, new double[cols], 0, freeVars, max);
-
-    return sum(varValues);
-}
-
-double[] findMissingVariables(double[][] augmented, double[] varValues, int depth, List<Integer> missingVars, int max) {
-    if (missingVars.size() == depth) {
-        return calculateVarValues(augmented, varValues);
-    }
-
-    var min = Integer.MAX_VALUE;
-    double[] minValues = null;
-    for (int i = 0; i < max; i++) {
-        varValues[missingVars.get(depth)] = i;
-        var vals = findMissingVariables(augmented, varValues, depth + 1, missingVars, max);
-        if (vals != null) {
-            var S = sum(vals);
-            if (min > S) {
-                min = S;
-                minValues = vals;
-                // todo: need to optimize this
-//                break;
-            }
-        }
-    }
-    return minValues;
-}
-
-double[] calculateVarValues(double[][] augmented, double[] varValues) {
-    var rows = augmented.length;
-    var cols = augmented[0].length - 1;
-    var vals = Arrays.copyOf(varValues, varValues.length);
-
-    for (var i = rows - 1; i >= 0; i--) {
-        var pivot = -1;
-        for (int j = 0; j < cols; j++) {
-            if (Math.abs(augmented[i][j]) > EPSILON) {
-                pivot = j;
-                break;
-            }
-        }
-        if (pivot != -1) {
-            vals[pivot] = augmented[i][cols];
-            for (int j = pivot + 1; j < cols; j++) {
-                vals[pivot] -= vals[j] * augmented[i][j];
-            }
-            if (!valid(vals[pivot])) {
-                return null;
-            }
-        }
-    }
-
-    return vals;
-}
-
-boolean valid(double val) {
-    if (val < -EPSILON) {
-        return false;
-    }
-
-    var r = Math.round(val);
-    return !(Math.abs(val - r) > EPSILON);
-}
-
-int sum(double[] values) {
-    double sum = 0D;
-    for (var v : values) {
-        sum += v;
-    }
-    return (int) sum;
-}
-
-
-int solveWithZ3(Machine m) {
-    HashMap<String, String> cfg = new HashMap<>();
-    cfg.put("model", "true");
-
-    try (var ctx = new Context(cfg)) {
-        var o = ctx.mkOptimize();
-        var vars = new ArrayList<IntExpr>();
-        var buttons = new ArrayList<>(m.buttons());
-        for (int i = 0; i < buttons.size(); i++) {
-            var v = ctx.mkIntConst("B" + i);
-            o.Add(ctx.mkGe(v, ctx.mkInt(0)));
-            vars.add(v);
-        }
-        for (int i = 0; i < m.joltage().size(); i++) {
-            var joltage = m.joltage().get(i);
-            var equation = new ArrayList<IntExpr>();
-            for (int j = 0; j < buttons.size(); j++) {
-                var button = buttons.get(j);
-                if (button.lights.contains(i)) {
-                    equation.add(vars.get(j));
+                if (invalid(c)) {
+                    return Integer.MAX_VALUE;
                 }
+
+                total += c;
             }
-            var e = ctx.mkAdd(equation.toArray(IntExpr[]::new));
-            o.Add(ctx.mkEq(ctx.mkAdd(e), ctx.mkInt(joltage)));
-        }
-        var min = o.MkMinimize(ctx.mkAdd(vars.toArray(IntExpr[]::new)));
-        if (Status.UNSATISFIABLE.equals(o.Check())) {
-            throw new AssertionError();
+
+            return (int) total;
         }
 
-        return Integer.parseInt(min.toString());
+        var min = Integer.MAX_VALUE;
+        for (var i = 0; i < max; i++) {
+            freeValues[depth] = i;
+            var res = minimize(A, freeValues, depth + 1, max);
+            min = Math.min(min, res);
+        }
+
+        return min;
+    }
+
+    /**
+     * 'You have to push each button an integer number of times;
+     *  there's no such thing as "0.5 presses" (nor can you push
+     *  a button a negative number of times)'
+     * A value is considered invalid in the following cases:
+     * - the value is lower than zero (negative)
+     * - the value is not a whole int
+     * @param val the variable value to check
+     * @return true if the given value is invalid, false otherwise
+     */
+    boolean invalid(double val) {
+        return val < -EPSILON || Math.abs(val - Math.rint(val)) > EPSILON;
+    }
+
+    double sum(double[] values) {
+        double sum = 0D;
+        for (var v : values) {
+            sum += v;
+        }
+        return sum;
+    }
+
+    int max(List<Integer> values) {
+        int max = Integer.MIN_VALUE;
+        for (var v : values) {
+            max = Math.max(max, v);
+        }
+        return max;
     }
 }
 
